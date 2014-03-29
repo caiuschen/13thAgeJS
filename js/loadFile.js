bf95ee92be7724b8c0e2c673983af292761779ea
caiuschen <chendz@cs.washington.edu>
2014-03-29 09:11:10 -0700

    Add ability to save a file.

--- js/loadFile.js
+++ js/loadFile.js
@@ -18,3 +18,19 @@ function handleFileSelect(evt) {
     // Read in JSON as a data URL.
     reader.readAsText(f, 'UTF-8');
 }
+
+function saveTextAsFile() {
+    var blob = new Blob(["Hi"], {type:"text/plain"});
+    var downloadLink = document.createElement("a");
+    downloadLink.download = "test.txt";
+    downloadLink.innerHTML = "Download File";
+    downloadLink.href = window.URL.createObjectURL(blob);
+    downloadLink.onclick = destroyClickedElement;
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+}
+
+function destroyClickedElement(e) {
+    document.body.removeChild(e.target);
+}
