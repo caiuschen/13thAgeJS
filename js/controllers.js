9cfba90adf2ad3544154a4ad6481e12a161ad3d0
caiuschen <chendz@cs.washington.edu>
2014-04-13 23:02:27 -0700

    Switching to more Angular-style JS

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,77 @@
-var classesApp = angular.module('classesApp', []);
+var characterBuilderApp = angular.module('characterBuilderApp', []);
  
-classesApp.controller('ClassesListCtrl', function ($scope, $http) {
-   $http.get('json/classes.json').success(function(data) {
-       $scope.classes = data;
-       console.log(data);
-   });
+characterBuilderApp.controller('CharacterBuilderCtrl', function ($scope, $http) {
+    $http.get('json/classes.json').success(function(data) {
+        $scope.classes = data;
+    });
+
+    $scope.character = {
+        characterName : "No character name",
+        playerName : "No player name",
+        oneUniqueThing : "No one unique thing",
+        level : 1,
+        baseAbilities : {
+            strength : 8,
+            constitution : 8,
+            dexterity : 8,
+            intelligence : 8,
+            wisdom : 8,
+            charisma : 8
+        },
+        classSelection : {
+            name : "No class selected",
+            chosenAbilityAdjustments : [],
+            classTalentSelections : []
+        },
+        raceSelection : {
+            name : "No race selected",
+            chosenAbilityAdjustment : null,
+            powerSelection : null
+        },
+        powerSelections : [],
+        armorLevel : "light",
+        hasShield : false
+    };
+
+    $scope.saveCharacter = function saveCharacter() {
+        var destroyClickedElement = function destroyClickedElement(e) {
+            document.body.removeChild(e.target);
+        };
+        var characterInStringForm = JSON.stringify(this.character);
+        var blob = new Blob([characterInStringForm], {type:"text/plain"});
+        var downloadLink = document.createElement("a");
+        downloadLink.download = "character.txt";
+        downloadLink.innerHTML = "Download File";
+        downloadLink.href = window.URL.createObjectURL(blob);
+        downloadLink.onclick = destroyClickedElement;
+        downloadLink.style.display = "none";
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+    }
+
+    $scope.loadCharacter = function loadCharacter(evt) {
+        var validateCharacter = function _validateCharacter(someCharacter) { };
+        var files = evt.target.files; // FileList object
+        f = files[0];
+        var reader = new FileReader();
+    
+        // Closure to capture the file information.
+        reader.onload = (function (theFile) {
+            return function (e) { 
+                var jsonObj = e.target.result
+                var parsedJSON = JSON.parse(jsonObj);
+                validateCharacter(parsedJSON);
+                $scope.character = parsedJSON;
+                $scope.outputToDebugSection(JSON.stringify(parsedJSON));
+            };
+        })(f);
+    
+        // Read in JSON as a data URL.
+        reader.readAsText(f, 'UTF-8');
+    };
+
+    $scope.outputToDebugSection = function outputToDebugSection(text) {
+        var debugSection = document.getElementById("debugOutput");
+        debugSection.innerHTML = text;
+    };
 });
--- js/loadFile.js
+++ js/loadFile.js
@@ -1,7 +1,33 @@
 var characterBuilder = {};
-characterBuilder.character = {};
+characterBuilder.character = {
+    characterName : "No character name",
+    playerName : "No player name",
+    oneUniqueThing : "No one unique thing",
+    level : 1,
+    baseAbilities : {
+        strength : 8,
+        constitution : 8,
+        dexterity : 8,
+        intelligence : 8,
+        wisdom : 8,
+        charisma : 8
+    },
+    classSelection : {
+        name : "No class selected",
+        chosenAbilityAdjustments : [],
+        classTalentSelections : []
+    },
+    raceSelection : {
+        name : "No race selected",
+        chosenAbilityAdjustment : null,
+        powerSelection : null
+    },
+    powerSelections : [],
+    armorLevel : "light",
+    hasShield : false
+};
 
-characterBuilder.validateCharacter = function validateCharacter(someCharacter) { };
+characterBuilder._validateCharacter = function _validateCharacter(someCharacter) { };
 
 characterBuilder.loadCharacter = function loadCharacter(evt) {
     var files = evt.target.files; // FileList object
@@ -13,8 +39,9 @@ characterBuilder.loadCharacter = function loadCharacter(evt) {
         return function (e) { 
             var jsonObj = e.target.result
             var parsedJSON = JSON.parse(jsonObj);
-            characterBuilder.validateCharacter(parsedJSON);
+            characterBuilder._validateCharacter(parsedJSON);
             characterBuilder.character = parsedJSON;
+            characterBuilder.outputToDebugSection(JSON.stringify(parsedJSON));
         };
     })(f);
 
@@ -22,19 +49,9 @@ characterBuilder.loadCharacter = function loadCharacter(evt) {
     reader.readAsText(f, 'UTF-8');
 };
 
-characterBuilder.saveCharacter = function saveCharacter() {
-    var destroyClickedElement = function destroyClickedElement(e) {
-        document.body.removeChild(e.target);
-    };
-    var characterInStringForm = JSON.stringify(characterBuilder.character);
-    var blob = new Blob([characterInStringForm], {type:"text/plain"});
-    var downloadLink = document.createElement("a");
-    downloadLink.download = "test.txt";
-    downloadLink.innerHTML = "Download File";
-    downloadLink.href = window.URL.createObjectURL(blob);
-    downloadLink.onclick = destroyClickedElement;
-    downloadLink.style.display = "none";
-    document.body.appendChild(downloadLink);
-    downloadLink.click();
-}
+characterBuilder.outputToDebugSection = function outputToDebugSection(text) {
+    var debugSection = document.getElementById("debugOutput");
+    debugSection.innerHTML = text;
+};
+
 
